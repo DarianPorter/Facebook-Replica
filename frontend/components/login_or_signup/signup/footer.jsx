4710f314b1caf0f204f6eb04977f6e627770e4f3
darianporter <darianporter@icloud.com>
2019-06-11 09:16:41 -0400

    footer styling

--- frontend/components/login_or_signup/signup/footer.jsx
+++ frontend/components/login_or_signup/signup/footer.jsx
@@ -3,7 +3,29 @@ import React from 'react'
 export default ()=>{
     return(
         <div className="footer">
+            <div className="footer-content">
+                <div className="lan">
+                    <ul>
+                        <li id="eng"> English (US)</li>
+                        <li>Français (France)</li>
+                        <li>Español</li>
+                        <li>中文(简体)</li>
+                        <li>العربية</li>
+                        <li>Português (Brasil)</li>
+                        <li>Italiano</li>
+                        <li>한국어</li>
+                        <li>Deutsch</li>
+                        <li>हिन्दी</li>
+                        <li>日本語</li>
+                    </ul>
+                </div>
+                <div className="seperator">
 
+                </div>
+                <div className="ui-list">   
+
+                </div>
+            </div>
         </div>
     )
 }
